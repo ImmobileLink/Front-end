ead765e2eba1bd1b7d11fa2fba6a760eb4dfb6db
LuanGrod <luan14rodrigues17@gmail.com>
2023-05-08 19:14:58 -0300

    Login - form, resp, theme - não implementado

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primaria': '#EE7624',
+        'secundaria': { '100': '#2563EB', '200': '#1654DB' },
+        'escuro': '#2A3A49',
+        'escuro2': '#1A2939',
+        'amarela': '#F9BF3A',
+        'branco': '#E4E4E4'
+      },
+      borderRadius: {
+        'giga': "70px"
+      }
+    },
+  },
+  plugins: [],
+}
