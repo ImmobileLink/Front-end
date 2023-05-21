e28f91d6026dc11814e4a84ae95044af30412743
Rhenan <gomesrhenan@gmail.com>
2023-05-21 12:35:59 -0300

    Deleção dos arquivos desnecessários resultantes do merge

--- src/lib/supabase-image-loader.js
+++
@@ -1,5 +0,0 @@
-const projectId = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID // seu id de projeto Supabase
-
-export default function supabaseLoader({ src, width, quality }) {
-  return `https://${projectId}.supabase.co/storage/v1/object/public/${src}?width=${width}&quality=${quality || 75}`
-}
