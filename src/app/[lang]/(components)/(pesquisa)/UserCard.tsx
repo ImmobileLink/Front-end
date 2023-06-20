f3898c26e0e003a89d099b1ead7864b55b331c6c
Rhenan <gomesrhenan@gmail.com>
2023-06-20 11:10:21 -0300

    Ajustes na página de pesquisa

--- src/app/[lang]/(components)/(pesquisa)/UserCard.tsx
+++ src/app/[lang]/(components)/(pesquisa)/UserCard.tsx
@@ -9,27 +9,34 @@ interface UserCardProps {
 }
 
 export default function UserCard({corretor, corporacao}: UserCardProps) {
+    
   return (
-    <div className="flex w-auto xl:w-2/3 xl m-4 p-5 bg-gray-600 shadow-md rounded-md my-5">
-      
-      <div className="flex-col w-100 justify-end"> 
-        {
-          corretor != null ?      
-          <div>
+    <div className="w-5/12 h-72 max-w-lg m-4 p-5 bg-gray-600 shadow-md rounded-md my-5">   
+      {
+          corretor != null ?
+            corretor.nota != null ?
+            <div>
+              <Avatar userId={corretor?.id}/>
+              <p>Nome: {corretor.nome}</p>
+              <p>CRECI: {corretor.creci}</p>
+              <p>Avaliaçao: {corretor.nota}</p>
+            </div>
+            :
+            <div>
             <Avatar userId={corretor?.id}/>
             <p>Nome: {corretor.nome}</p>
             <p>CRECI: {corretor.creci}</p>
-            <p>Avaliaçao: {corretor.nota}</p>
-          </div> 
+            <p>Avaliaçao: N/A</p>
+          </div>  
           :
           <div>
             <Avatar userId={corporacao?.id}/>
             <p>Nome: {corporacao?.nomefantasia}</p>
-            <p>Região: {corporacao?.regiao}</p>
+            <p>CNPJ: {corporacao?.cnpj}</p>
           </div> 
         }
                   
-        <div className="flex flex-row mt-5">
+        <div className="flex flex-row align-bottom mt-5">
           <div>
             <button className="mr-1 p-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 justify-center text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg">
               Iniciar Chat
@@ -40,8 +47,7 @@ export default function UserCard({corretor, corporacao}: UserCardProps) {
               Ver perfil
             </button>
           </div>
-        </div>
-      </div>
+        </div>  
     </div>
   );
 }
