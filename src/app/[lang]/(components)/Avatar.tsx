003834ba2eabfa025a212e8f73635e51402ae297
LuanGrod <luan14rodrigues17@gmail.com>
2023-06-24 15:34:42 -0300

    cards feed: perfil, botões de navegação e conexões

--- src/app/[lang]/(components)/Avatar.tsx
+++ src/app/[lang]/(components)/Avatar.tsx
@@ -1,35 +1,35 @@
-"use client"
+"use client";
 import Image from "next/image";
-import { Suspense, useState } from "react"
+import { Suspense, useState } from "react";
 
 interface AvatarProps {
-    userId: any;
-    size?: String;
+  userId: any;
+  size?: String;
 }
 
-export default function Avatar({userId, size}: AvatarProps) {
-    let styleImage = ""
+export default function Avatar({ userId, size }: AvatarProps) {
+  let styleImage = "";
 
-    if(size == "big"){
-        styleImage = "w-32 h-32 rounded-full ring-1 ring-gray-500"
-    }else{
-        styleImage = "h-14 w-14 rounded-full ring-1 ring-gray-500"
-    }
+  if (size == "big") {
+    styleImage = "w-32 h-32 m-1 rounded-full ring-1 ring-gray-200";
+  } else {
+    styleImage = "h-14 w-14 m-1 rounded-full ring-1 ring-gray-200";
+  }
 
-    const [src, setSrc] = useState(`users/profile_picture/${userId}`);  
+  const [src, setSrc] = useState(`users/profile_picture/${userId}`);
 
-        return (
-            <div>
-                <Suspense fallback={<span>loading...</span>}/>  
-                <Image
-                    className={styleImage}
-                    src={src}
-                    width={1}
-                    height={1}
-                    onError={() => setSrc('/users/nopfp')}
-                    alt="Profile Picture"
-                />                    
-            </div>              
-        )
-    
+  return (
+    <div>
+      <Suspense fallback={<span>loading...</span>}>
+        <Image
+          className={styleImage}
+          src={src}
+          width={1}
+          height={1}
+          onError={() => setSrc("/users/nopfp")}
+          alt="Profile Picture"
+        />
+      </Suspense>
+    </div>
+  );
 }
