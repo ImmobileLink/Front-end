419a93bdd3f8c2ccd689d68e670341b8f477731c
LuanGrod <luan14rodrigues17@gmail.com>
2023-06-25 15:03:09 -0300

    feed - cards de conexões

--- src/app/[lang]/(components)/(feed)/CardAmizade.tsx
+++ src/app/[lang]/(components)/(feed)/CardAmizade.tsx
@@ -5,11 +5,12 @@ import Avatar from "../Avatar";
 import Link from "next/link";
 
 interface CardAmizadeProps {
-  id: string;
+  idremetente: string | undefined;
+  iddestinatario: string;
   nome: string;
 }
 
-export default function CardAmizade({ id, nome }: CardAmizadeProps) {
+export default function CardAmizade({ idremetente, iddestinatario, nome }: CardAmizadeProps) {
 
   const handleEnviarMensagem = () => {
     console.log("oi")
@@ -18,15 +19,15 @@ export default function CardAmizade({ id, nome }: CardAmizadeProps) {
   return (
     <>
       <div
-        key={id}
+        key={iddestinatario}
         className="flex justify-between mx-6 gap-2 ring-1 ring-black ring-inset rounded-full"
       >
         <div className="flex">
-          <Avatar userId={id} />
-          <Link className="self-center capitalize" href={`/perfil/${id}`}>{nome}</Link>
+          <Avatar userId={iddestinatario} />
+          <Link className="self-center capitalize text-black" href={`/perfil/${iddestinatario}`}>{nome}</Link>
         </div>
-        <div className="flex justify-center align-middle self-center w-14 h-14 mr-1 rounded-full hover:bg-orange-200 ease-in-out duration-500 hover:cursor-pointer" onClick={handleEnviarMensagem}>
-          <FiMail className="self-center text-xl" />
+        <div className="flex justify-center align-middle self-center w-14 h-14 mr-1 rounded-full hover:cursor-pointer" onClick={handleEnviarMensagem}>
+          <FiMail className="self-center text-xl text-black" />
         </div>
       </div>
     </>
