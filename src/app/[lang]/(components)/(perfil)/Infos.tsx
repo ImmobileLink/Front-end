5d4bc076955c71322a740ba895c4314d0ca3b0c2
ArthurGoncalves1 <arth09th@gmail.com>
2023-06-25 17:42:06 -0300

    perfil busca infos no BD e mudanca dashboard

--- src/app/[lang]/(components)/(perfil)/Infos.tsx
+++ src/app/[lang]/(components)/(perfil)/Infos.tsx
@@ -7,7 +7,7 @@ interface InfosProps {}
 export default function Infos({}: InfosProps) {
   return (
     <>
-      <div className="w-auto bg-white h-48 mt-10 rounded-md m-3 overflow-hidden">
+      <div className="w-auto bg-white h-48 mt-5 rounded-md m-3 overflow-hidden">
         <div className="flex justify-around items-center border-b-2 font-semibold h-9">
           <button className="w-1/2 h-full text-center border-r-2 hover:bg-slate-100">Visão Geral</button>
           <button className="w-1/2 h-full text-center hover:bg-slate-100">Publicações</button>
