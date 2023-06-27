24d69252195fd887821c5e3e4e05eeda318bc9db
ArthurGoncalves1 <arth09th@gmail.com>
2023-06-27 10:51:23 -0300

    botao e plano

--- src/app/[lang]/perfil/[id]/page.tsx
+++ src/app/[lang]/perfil/[id]/page.tsx
@@ -6,7 +6,6 @@ import Dashboard from "../../(components)/(perfil)/Dashboard";
 import Infos from "../../(components)/(perfil)/Infos";
 import type { Database } from '../../../../../lib/database.types';
 import NavBar from '../../(components)/NavBar';
-import { isatty } from 'tty';
 import Calendario from '../../(components)/Calendario';
 
 interface pageProps {
@@ -28,13 +27,18 @@ export default async function Page({ params: { id } }: pageProps) {
        valor2: session!.user.id
      })
 
-     let { data: user_data } = await supabase
+     let { data: session_data } = await supabase
      .from('simple_user_data')
      .select('*')
      .eq('id', session?.user.id)
      .single()
 
-     console.log("user: " + user_data)
+     let { data: corretor } = await supabase
+     .from('corretor')
+     .select('*')
+     .eq('id', id)
+     .single()
+
 
 
   return (
@@ -44,19 +48,19 @@ export default async function Page({ params: { id } }: pageProps) {
         <div className="h-screen w-screen">
           <div className="flex relative max-w-6xl mx-auto px-4 mt-4">
 
-            <div className="w-2/3 bg-branco mr-3 rounded-md overflow-hidden h-screen">
-              <Cabecalho isAssociado={isAssociado} idProfile={id} user_data={user_data} />
-              <Infos />
+            <div className="w-2/3 relative  bg-branco mr-3 rounded-md overflow-hidden h-screen">
+              <Cabecalho isAssociado={isAssociado} session_data={session_data} corretor={corretor}/>
+              <Infos corretor={corretor}/>
             </div>
 
 
 
             <div className="w-1/3 ">
               <div className="bg-branco rounded-md p-3 relative overflow-hidden">
-                <Dashboard userId={id} session={session} premium={user_data!.premium}/>
+                <Dashboard userId={id} session={session} premium={session_data!.premium}/>
               </div>
 
-              {isAssociado == "true" ? (
+              {isAssociado == "Associado" ? (
                   <div className="bg-branco rounded-md mt-3 p-3">
                   <Calendario /> 
                 </div>
