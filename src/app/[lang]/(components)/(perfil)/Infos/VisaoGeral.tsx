f84237c12c3a9ae680924b5570d0083587cdb33d
ArthurGoncalves1 <arth09th@gmail.com>
2023-06-28 10:46:18 -0300

    perfil mockado e internacionalizacao

--- src/app/[lang]/(components)/(perfil)/Infos/VisaoGeral.tsx
+++ src/app/[lang]/(components)/(perfil)/Infos/VisaoGeral.tsx
@@ -3,6 +3,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "../../../../../../lib/database.types";
 import { cookies } from "next/headers";
 import { useEffect, useState } from "react";
+import Historico from "./historico/Historico";
 
 interface VisaoGeralProps {
   corretor: any;
@@ -23,26 +24,38 @@ export default function VisaoGeral({ corretor }: VisaoGeralProps) {
     fetchData()
   }, [])
 
-
   return (
     <div className="ml-6">
-      <h2 className="font-semibold text-xl mb-3 text-center">Informações sobre o corretor</h2>
-      <div>
-        <p><strong>Creci</strong></p>
-        <p>{corretor.creci}</p>
-      </div>
-      {especialidades ? (
-        <div className="mt-3">
-          <p><strong>Especialidades</strong></p>
-          <ul className="list-disc ml-8">
-            {especialidades?.map((item, index) => (
-              <li key={index}>{item.descricao}</li>
-            ))}
-          </ul>
+      <h2 className="font-semibold text-xl mb-5">Informações sobre o corretor</h2>
+      <div className="flex items-start justify-between  w-3/5">
+        <div>
+          <p className="font-semibold ">Creci</p>
+          <p>{corretor.creci}</p>
         </div>
-      ) : (<></>)}
+        {corretor.numero ? (
+          <div className="">
+            <p className="font-semibold ">Telefone</p>
+            <p>{corretor.numero}</p>
+          </div>
+        ) : (<></>)}
+
+        {especialidades ? (
+          <div className="">
+            <p className="font-semibold">Especialidades</p>
+            <ul className="list-disc ml-8">
+              {especialidades?.map((item, index) => (
+                <li key={index}>{item.descricao}</li>
+              ))}
+            </ul>
+          </div>
+        ) : (<></>)}
+      </div>
 
 
+      <div className="mt-5">
+        <h2 className="font-semibold text-xl">Histórico</h2>
+        <Historico />
+      </div>
 
     </div>
 
