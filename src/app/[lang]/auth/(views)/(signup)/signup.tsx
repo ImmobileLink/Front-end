41b9a2b1cbd90cfb7bdcaa90692e24f0985e141e
BeatrizNataly <beatriznatyce@gmail.com>
2023-08-16 18:52:25 -0300

    Delegação de validações para o server-side

--- src/app/[lang]/auth/(views)/(signup)/signup.tsx
+++ src/app/[lang]/auth/(views)/(signup)/signup.tsx
@@ -18,6 +18,13 @@ import Signup5 from "./signup5";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "../../../../../../lib/database.types";
 import Loading from "@/app/[lang]/(components)/(auth)/Loading";
+import { handleSignUpDB, verifyFields } from "./utils";
+
+/**
+ * TO DO:
+ * Mover dados sensíveis que acessem o servidor para um componente server side
+ * Ajustar padding do seletor de persona do cadastro
+ */
 
 interface SignUpProps {
     setAlert: Dispatch<
@@ -98,7 +105,25 @@ export default function SignUp({ setAlert, signup, data, lang }: SignUpProps) {
             if (telaAtual < 5) {
                 isLoading(true);
             }
-            if (await verifyFields()) {
+            if (
+                await verifyFields(
+                    telaAtual,
+                    setAlert,
+                    signup,
+                    tipoPerfil,
+                    senha,
+                    email,
+                    supabase,
+                    nome,
+                    cpf,
+                    cnpj,
+                    nomeFantasia,
+                    celular,
+                    telefone,
+                    comercial,
+                    creci
+                )
+            ) {
                 setAlert({
                     type: "warning",
                     title: "",
@@ -118,305 +143,37 @@ export default function SignUp({ setAlert, signup, data, lang }: SignUpProps) {
         // }
     };
 
-    const verifyFields = async () => {
-        switch (telaAtual) {
-            case 1:
-                // verifica senha
-                if (!hasStrongPassword(senha)) {
-                    setAlert({
-                        type: "warning",
-                        title: "",
-                        message: signup.signup1.logs.invalidpassword,
-                    });
-                    return false;
-                }
-
-                // verifica email
-                const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{1,}$/;
-                if (!regex.test(email)) {
-                    setAlert({
-                        type: "warning",
-                        title: "",
-                        message: signup.signup1.logs.invalidemail,
-                    });
-
-                    return false;
-                }
-
-                let { data: usuario } = await supabase
-                    .from("usuario")
-                    .select("email")
-                    .eq("email", email);
-
-                if (usuario?.length) {
-                    setAlert({
-                        type: "warning",
-                        title: "",
-                        message: signup.signup1.logs.emailalreadyused,
-                    });
-                    return false;
-                }
-                return true;
-            case 2:
-                // Nenhuma validação necessária no passo 2 por enquanto.
-                return true;
-            case 3:
-                if (tipoPerfil == 1) {
-                    // valida nome
-                    if (nome.length < 4) {
-                        setAlert({
-                            type: "warning",
-                            title: "",
-                            message: signup.signup3.logs.invalidname,
-                        });
-                        return false;
-                    }
-                    // valida cpf
-                    if (cpf.length != 11) {
-                        setAlert({
-                            type: "warning",
-                            title: "",
-                            message: signup.signup3.logs.invalidcpf,
-                        });
-                        return false;
-                    }
-
-                    let { data: usuario } = await supabase
-                        .from("corretor")
-                        .select("cpf")
-                        .eq("cpf", cpf);
-
-                    if (usuario?.length) {
-                        setAlert({
-                            type: "warning",
-                            title: "",
-                            message: signup.signup3.logs.invalidcpf,
-                        });
-                        return false;
-                    }
-
-                    // valida cnpj
-                    if (cnpj.length != 14 && cnpj.length > 0) {
-                        setAlert({
-                            type: "warning",
-                            title: "",
-                            message: signup.signup3.logs.invalidcnpj,
-                        });
-                        return false;
-                    } else if (cnpj.length == 14) {
-                        let { data: usuario } = await supabase
-                            .from("corretor")
-                            .select("cnpj")
-                            .eq("cnpj", cnpj);
-
-                        if (usuario?.length) {
-                            setAlert({
-                                type: "warning",
-                                title: "",
-                                message: signup.signup3.logs.invalidcnpj,
-                            });
-                            return false;
-                        }
-                    }
-                } else {
-                    // o nome fantasia tem no mínimo 12 pontos [LegisWEB]
-                    if (nomeFantasia.length < 12) {
-                        setAlert({
-                            type: "warning",
-                            title: "",
-                            message: signup.signup3.logs.invalidfantasyname,
-                        });
-                        return false;
-                    }
-                    if (cnpj.length != 14) {
-                        setAlert({
-                            type: "warning",
-                            title: "",
-                            message: signup.signup3.logs.invalidcnpj,
-                        });
-                        return false;
-                    } else {
-                        let { data: usuario } = await supabase
-                            .from("corporacao")
-                            .select("cnpj")
-                            .eq("cnpj", cnpj);
-
-                        if (usuario?.length) {
-                            setAlert({
-                                type: "warning",
-                                title: "",
-                                message: signup.signup3.logs.invalidcnpj,
-                            });
-                            return false;
-                        }
-                    }
-                }
-                if (
-                    (celular.length != 11 && celular.length != 0) ||
-                    (telefone.length != 10 && telefone.length != 0) ||
-                    (comercial.length != 10 && comercial.length != 0)
-                ) {
-                    setAlert({
-                        type: "warning",
-                        title: "",
-                        message: signup.signup3.logs.invalidphone,
-                    });
-                    return false;
-                }
-                return true;
-            case 4:
-                if (tipoPerfil == 1) {
-                    if (creci.length < 7) {
-                        setAlert({
-                            type: "warning",
-                            title: "",
-                            message: signup.signup4.logs.invalidcreci,
-                        });
-                        return false;
-                    } else {
-                        const regexCreci = /^\d{6}[a-zA-Z]$/;
-                        if (!regexCreci.test(creci)) {
-                            setAlert({
-                                type: "warning",
-                                title: "",
-                                message: signup.signup4.logs.invalidcreci,
-                            });
-                            return false;
-                        }
-                        let { data: usuario } = await supabase
-                            .from("corretor")
-                            .select("creci")
-                            .eq("creci", creci);
-
-                        if (usuario?.length) {
-                            setAlert({
-                                type: "warning",
-                                title: "",
-                                message: signup.signup4.logs.invalidcreci,
-                            });
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            case 5:
-                // Nenhuma validação necessária no passo 5 por enquanto.
-                return true;
-        }
-        // default
-        return false;
-    };
-
-    function hasStrongPassword(object: string) {
-        const password = object;
-        const minLength = 6;
-        const hasUppercase = /[A-Z]/.test(password);
-        const hasLowercase = /[a-z]/.test(password);
-        const hasNumber = /[0-9]/.test(password);
-        const hasSpecialChar = /[!@#$%^&*()\-=_+[\]{}|\\;:'",.<>/?]/.test(
-            password
-        );
-
-        if (
-            password.length >= minLength &&
-            hasUppercase &&
-            hasLowercase &&
-            hasNumber &&
-            hasSpecialChar
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     const handleSignUp = async () => {
-        let { data, error } = await supabase.auth.signUp({
-            email: email,
-            password: senha,
-            options: {
-                emailRedirectTo: `${location.origin}/auth/callback`,
-            },
-        });
-
-        if (!error) {
-            const userId = data.user?.id!;
-
-            if (tipoPerfil == 1) {
-                let { error } = await supabase.from("corretor").insert([
-                    {
-                        id: userId,
-                        nome: nome,
-                        cpf: cpf,
-                        cnpj: cnpj,
-                        creci: creci,
-                        cep: cep,
-                        estado: estado,
-                        cidade: cidade,
-                        bairro: bairro,
-                        logradouro: logradouro,
-                        numero: numero,
-                        complemento: complemento,
-                        telefone: telefone,
-                        celular: celular,
-                        comercial: comercial,
-                        premium: premium,
-                    },
-                ]);
-                // especialidade
-                {
-                    let arrayEspecialidade: any = [];
-
-                    especialidade.map((item) => {
-                        arrayEspecialidade.push({
-                            idcorretor: userId,
-                            idtipoimovel: item.id,
-                        });
-                    });
-
-                    const { error } = await supabase
-                        .from("especialidade")
-                        .insert(arrayEspecialidade);
-                }
-            } else {
-                let { error } = await supabase.from("corporacao").insert([
-                    {
-                        id: userId,
-                        nomefantasia: nomeFantasia,
-                        cnpj: cnpj,
-                        cep: cep,
-                        estado: estado,
-                        cidade: cidade,
-                        bairro: bairro,
-                        logradouro: logradouro,
-                        numero: numero,
-                        complemento: complemento,
-                        telefone1: telefone,
-                        telefone2: celular,
-                        telefone3: comercial,
-                        premium: premium,
-                    },
-                ]);
-            }
-            //usuarioporregiao
-            {
-                let arrayUsuarioPorRegiao: any = [];
-
-                regiaoAtuacao.map((item) => {
-                    arrayUsuarioPorRegiao.push({
-                        idusuario: userId,
-                        idregiao: item.id,
-                    });
-                });
-
-                const { error } = await supabase
-                    .from("usuarioporregiao")
-                    .insert(arrayUsuarioPorRegiao);
-            }
-
+        const callback = handleSignUpDB(
+            tipoPerfil,
+            senha,
+            email,
+            supabase,
+            nome,
+            cpf,
+            cnpj,
+            nomeFantasia,
+            celular,
+            telefone,
+            comercial,
+            creci,
+            cep,
+            estado,
+            logradouro,
+            numero,
+            complemento,
+            cidade,
+            bairro,
+            premium,
+            especialidade,
+            regiaoAtuacao
+        );
+        if (await callback) {
             setIsOK(true);
-
             router.refresh();
+        } else {
+            //Algo deu errado [Investigar!]
+            console.log("handleSignUpDB error!")
         }
     };
 
