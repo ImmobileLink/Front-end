8029a4fa7a27e7e222d98dc7ce8e944a27a4fa01
BeatrizNataly <beatriznatyce@gmail.com>
2023-08-23 20:01:14 -0300

    Terms of use hardcoded dummy added

--- src/app/[lang]/auth/AuthForm.tsx
+++ src/app/[lang]/auth/AuthForm.tsx
@@ -43,6 +43,119 @@ export default function AuthForm({ auth, data, lang }: AuthFormProps) {
                     setAlert={setAlert}
                     forgetpassword={auth.forgetpassword}
                 />
+            ) : view == "terms" ? (
+                <>
+                    <div className="h-96 md:h-80 overflow-y-scroll">
+                        {
+                            // os termos daqui são um placeholder - HARDCODED
+                        }
+                        <h1 className="text-xl text-bold">
+                            TERMOS DE USO DA PLATAFORMA
+                        </h1>
+                        <br />
+                        <b>
+                            Aviso Importante: Projeto de Conclusão de Curso -
+                            Uso Simulado e Fictício
+                        </b>
+
+                        <p>
+                            Este site é parte de um projeto de conclusão de
+                            curso e foi desenvolvido com o propósito específico
+                            de apresentar o conceito de um canal de comunicação
+                            de corretores com imobiliárias. Por favor, tenha em
+                            mente as seguintes considerações ao utilizar esta
+                            plataforma:
+                        </p>
+                        <br />
+                        <p>
+                            <b>Simulação e Ficção: </b>
+                            Esta plataforma não representa uma plataforma real
+                            para corretores de imóveis e não está conectada a
+                            quaisquer transações ou atividades reais do mercado
+                            imobiliário. Todas as interações, anúncios, imóveis
+                            e informações contidas neste site são simuladas e
+                            fictícias, destinadas apenas para fins acadêmicos e
+                            de demonstração.
+                        </p>
+                        <br />
+                        <p>
+                            <b>Dados Fictícios: </b>
+                            Quaisquer informações fornecidas para cadastro,
+                            incluindo nome, endereço, números de telefone,
+                            e-mails e outros detalhes, devem ser inteiramente
+                            fictícios. Não é recomendado o uso de informações
+                            pessoais reais ou sensíveis. Não utilizamos de forma
+                            alguma as informações fornecidas neste site para
+                            fins que não sejam apenas para exibição e
+                            demonstração de suas funcionalidades.
+                        </p>
+                        <br />
+                        <p>
+                            <b>Uso Responsável: </b>
+                            Ao interagir com esta plataforma, solicitamos que
+                            você a utilize de forma responsável e respeitosa.
+                            Não é permitido o compartilhamento de conteúdo
+                            ofensivo, discriminatório, ilegal ou prejudicial.
+                        </p>
+                        <br />
+                        <p>
+                            <b>Direitos Autorais: </b>
+                            Todas as imagens, logotipos e outros materiais
+                            visuais utilizados neste site são usados com fins
+                            ilustrativos e de demonstração, e os direitos
+                            autorais pertencem aos seus respectivos
+                            proprietários.
+                        </p>
+                        <br />
+                        <p>
+                            <b>Feedback e Contato: </b>
+                            Caso deseje fornecer feedback sobre esta plataforma,
+                            por favor, esteja ciente de que qualquer comentário
+                            ou sugestão serão considerados para fins acadêmicos
+                            e de melhoria do projeto.
+                        </p>
+                        <br />
+                        <p>
+                            Ao prosseguir e utilizar este site, você reconhece
+                            que está ciente da natureza simulada e fictícia
+                            desta plataforma e concorda em respeitar as
+                            condições e orientações aqui apresentadas.
+                        </p>
+                        <br />
+                        <p>
+                            Para obter informações ou esclarecimentos adicionais
+                            sobre este projeto de conclusão de curso, entre em
+                            contato através do canal indicado no site.
+                            Agradecemos por sua compreensão e interesse em nosso
+                            projeto de conclusão de curso.
+                        </p>
+                        <br />
+                        <a>Atenciosamente,</a>
+                        <br />
+                        <a
+                            className="font-semibold text-blue-500"
+                            target="_blank"
+                            href="https://immobilelink.blogspot.com/"
+                        >
+                            Equipe ImmobileLink
+                        </a>
+                        <br />
+                        <br />
+                        <p>
+                            Tecnologia em Análise e Desenvolvimento de SIstemas
+                            - IFSP, 2023
+                        </p>
+                    </div>
+                    <div className="flex mt-10 w-full justify-center text-sm text-gray-500">
+                        <p>{auth.always.alreadyhaveanaccount}</p>
+                        <button
+                            onClick={() => handleChangeView("signin")}
+                            className="font-semibold ml-2 text-blue-500 hover:text-secundaria-100"
+                        >
+                            {auth.always.singin}
+                        </button>
+                    </div>
+                </>
             ) : (
                 <h1>ERRO</h1>
             )}
@@ -92,33 +205,37 @@ export default function AuthForm({ auth, data, lang }: AuthFormProps) {
                     <p></p>
                 )}
                 <div className="flex justify-center">
-                <div className="w-72 flex align-center text-center justify-center mt-10">
-                    <p className="text-gray-500 text-xs">
-                        {view == "signin" ? (
-                            <>
-                                {auth.always.bylogginin}
-                                <button
-                                    onClick={() => console.log("termos")}
-                                    className="font-semibold ml-1 mr-1 text-blue-500 hover:text-secundaria-100"
-                                >
-                                    {auth.always.terms}
-                                </button>{" "}
-                                {auth.always.ofthisplataform}
-                            </>
-                        ) : view == "signup" ? (
-                          <>
-                          {auth.always.bycreatinganaccount}
-                          <button
-                              onClick={() => console.log("termos")}
-                              className="font-semibold ml-1 mr-1 text-blue-500 hover:text-secundaria-100"
-                          >
-                              {auth.always.terms}
-                          </button>{" "}
-                          {auth.always.ofthisplataform}
-                      </>
-                        ) : null}
-                    </p>
-                </div>
+                    <div className="w-72 flex align-center text-center justify-center mt-10">
+                        <p className="text-gray-500 text-xs">
+                            {view == "signin" ? (
+                                <>
+                                    {auth.always.bylogginin}
+                                    <button
+                                        onClick={() =>
+                                            handleChangeView("terms")
+                                        }
+                                        className="font-semibold ml-1 mr-1 text-blue-500 hover:text-secundaria-100"
+                                    >
+                                        {auth.always.terms}
+                                    </button>{" "}
+                                    {auth.always.ofthisplataform}
+                                </>
+                            ) : view == "signup" ? (
+                                <>
+                                    {auth.always.bycreatinganaccount}
+                                    <button
+                                        onClick={() =>
+                                            handleChangeView("terms")
+                                        }
+                                        className="font-semibold ml-1 mr-1 text-blue-500 hover:text-secundaria-100"
+                                    >
+                                        {auth.always.terms}
+                                    </button>{" "}
+                                    {auth.always.ofthisplataform}
+                                </>
+                            ) : null}
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
