dd4c8df25af2dc479294aed6838ce4fcdfe3a12b
gomesrhenan <gomesrhenan@gmail.com>
2023-09-24 12:52:53 -0300

    Adição dos cabeçalhos next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,36 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; img-src *; script-src 'self';",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
