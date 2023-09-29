80844ec01306ed133664fca86248145d85141b87
BeatrizNataly <beatriznatyce@gmail.com>
2023-09-29 13:16:43 -0300

    Layout base concluído, mockups adicionados

--- src/app/[lang]/(components)/(survey)/radioSelector.tsx
+++ src/app/[lang]/(components)/(survey)/radioSelector.tsx
@@ -1,1 +1,51 @@
 // seletor de satisfação horizontal de 1 a 5...
+"use client";
+interface RadioSelectorProps {
+    params: {
+        lang: string;
+        pergunta: string;
+    };
+}
+
+export default function RadioSelector({
+    params: { lang, pergunta },
+}: RadioSelectorProps) {
+    const renderRadioOptions = () => {
+        let radios = [];
+
+        for (let i = 1; i <= 5; i++) {
+            radios.push(
+                <div className="justify-center align-center flex flex-col">
+                    <input
+                    className="w-8 h-8"
+                        type="radio"
+                        id={`op` + i}
+                        value={`op` + i}
+                        name={pergunta}
+                    />
+                    <label className="ml-[35%]" htmlFor={`op` + i}>{i}</label>
+                </div>
+            );
+        }
+
+        return radios;
+    };
+
+    return (
+        <>
+            <p className="py-2">
+                {pergunta}
+                <span className="text-primaria">{" *"}</span>
+            </p>
+            <div className="bg-branco dark:bg-dark-200 rounded px-2 py-2">
+                <div className="flex flex-row justify-between">
+                    {renderRadioOptions()}
+                </div>
+            </div>
+            <div className="flex flex-row justify-between">
+                <p>Nada satisfeito</p>
+                <p>Muito satisfeito</p>
+            </div>
+        </>
+    );
+}
--- src/app/[lang]/survey/page.tsx
+++ src/app/[lang]/survey/page.tsx
@@ -9,6 +9,7 @@ import { useState, Dispatch, SetStateAction } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Database } from "../../../../lib/database.types";
 import CardRoot from "../(components)/(compositions)/(card)/CardRoot";
+import RadioSelector from "../(components)/(survey)/radioSelector";
 // import { Database } from "../../../../../lib/database.types";
 // import PasswordInput from "../../(components)/(auth)/PasswordInput";
 // import Loading from "../../(components)/(auth)/Loading";
@@ -41,10 +42,65 @@ export default function Survey({ lang }: PageProps) {
                 </div>
             </nav>
             <div className="space-y-6 sm:mx-auto w-full md:px-12 px-4">
-                <div className="relative z-0 w-full mb-6 group py-4">
-                    <CardRoot>
-                    Pesquisa de Satisfação....
-                    <p>Lorem Ipsum...</p>
+                <div className="relative z-0 w-full group pt-4">
+                    <CardRoot className="max-h-[34rem] overflow-y-scroll">
+                        <div className="my-2 mx-2 md:my-4 md:mx-8">
+                            <h1 className="text-xl md:text-2xl my-2 md:my-4">
+                                BEM-VINDO(A) À PESQUISA DE SATISFAÇÃO DA
+                                IMMOBILELINK
+                            </h1>
+                            <p>
+                                Aqui você pode avaliar a sua experiência
+                                utilizando a plataforma e ajudar a tornar a
+                                ImmobileLink ainda melhor!
+                                <br />A pesquisa leva menos de 3 minutos para
+                                ser concluída, basta indicar em uma escala de 1
+                                a 5, o quão satisfeito está com nossos serviços,
+                                sendo 5 = Muito satisfeito e 1 = Nada
+                                satisfeito.
+                            </p>
+                            <form>
+                                <RadioSelector
+                                    params={{
+                                        lang: lang,
+                                        pergunta: "Pergunta 1",
+                                    }}
+                                />
+                                <RadioSelector
+                                    params={{
+                                        lang: lang,
+                                        pergunta: "Pergunta 2",
+                                    }}
+                                />
+                                <RadioSelector
+                                    params={{
+                                        lang: lang,
+                                        pergunta: "Pergunta 3",
+                                    }}
+                                />
+
+                                <div className="my-4">
+                                    <label className="py-2">
+                                        Compartilhe um pouco mais sobre sua
+                                        experiência:
+                                    </label>
+                                    <br />
+                                    <textarea
+                                        className="bg-branco dark:bg-dark-200 rounded h-36 md:h-16 w-full py-2 px-2"
+                                        id="op-comentario"
+                                    />
+                                </div>
+                                <div className="flex justify-end">
+                                    <button
+                                        // onClick={submitForm}
+                                        className="flex justify-center rounded-md bg-secundaria-100 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-secundaria-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secundaria-200"
+                                    >
+                                        {/* <Loading loading={loading} /> */}
+                                        FINALIZAR PESQUISA
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </CardRoot>
                 </div>
             </div>
