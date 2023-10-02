737643cacf188f282e97b5c3af4e9feb835c9398
gomesrhenan <gomesrhenan@gmail.com>
2023-10-02 08:23:30 -0300

    fix bug Utils

--- lib/utils/userData.ts
+++ lib/utils/userData.ts
@@ -1,7 +1,7 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { Database } from "../database.types";
-import { userData } from '../modelos';
+import { userData } from "../modelos";
 import { cache } from "react";
 
 export const createServerSupabaseClient = cache(() => {
