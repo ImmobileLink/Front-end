829f9500fbfeeea1eabca7d5ae6143fc47def521
ArthurGoncalves1 <arth09th@gmail.com>
2023-10-11 17:52:40 -0300

    EditImage

--- src/app/[lang]/perfil/[id]/(perfil)/components/Cabecalho/EditCapa.tsx
+++ src/app/[lang]/perfil/[id]/(perfil)/components/Cabecalho/EditCapa.tsx
@@ -1,11 +1,26 @@
+"use client"
+
+import { useState } from "react";
+import EditImage from "./EditImage";
+
 interface EditCapaProps { }
 
-export default async function EditCapa({ }: EditCapaProps) {
+export default function EditCapa({ }: EditCapaProps) {
+
+    const [editImage, setEditImage] = useState<boolean>(false)
+
     return (
         <>
-            <button className="absolute top-2 right-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            <button onClick={() => setEditImage(!editImage)} className="absolute top-2 right-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                 Alterar Imagem
             </button>
+
+            {/* {editImage && (
+                <EditImage />
+            )} */}
+
+            <EditImage />
+
         </>
     );
 }
