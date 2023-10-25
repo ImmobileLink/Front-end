95d2084d34c00ad16f294d5696878418a1a29f67
BeatrizNataly <beatriznatyce@gmail.com>
2023-10-25 17:31:39 -0300

    duplicata foto de perfil do user logado removida das sugestões de conexão

--- src/app/[lang]/(components)/(cards)/CardUserList.tsx
+++ src/app/[lang]/(components)/(cards)/CardUserList.tsx
@@ -1,47 +1,53 @@
 import { Cards } from "@/app/i18n/dictionaries/types";
 import Avatar from "../Avatar";
 import Link from "next/link";
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { Database } from '../../../../../lib/database.types';
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { Database } from "../../../../../lib/database.types";
 import { cache } from "react";
 
 interface CardUserListProps {
-  cards: Cards;
+    cards: Cards;
+    avatar: string | undefined;
 }
 
 export const createServerSupabaseClient = cache(() => {
-  const cookieStore = cookies()
-  return createServerComponentClient<Database>({ cookies: () => cookieStore })
-})
+    const cookieStore = cookies();
+    return createServerComponentClient<Database>({
+        cookies: () => cookieStore,
+    });
+});
 
-async function getData() {
-  const supabase = createServerSupabaseClient();
-  let { data, error } = await supabase.rpc("obter_cinco_corretores_id");
+async function getData(avatar: string | undefined) {
+    const supabase = createServerSupabaseClient();
+    let { data, error } = await supabase
+        .rpc("obter_cinco_corretores_id")
+        .neq("avatar", avatar);
 
-  return data;
+    return data;
 }
 
-export default async function CardUserList({ cards }: CardUserListProps) {
-  const avatares = await getData();
+export default async function CardUserList({
+    cards,
+    avatar,
+}: CardUserListProps) {
+    const avatares = await getData(avatar);
 
-  return (
-    <>
-      <div className="flex flex-col  justify-center">
-        <div className="flex mb-5 p-1 -space-x-4 justify-center">
-          {avatares?.map((item, index) => {
-            return (
-              <Avatar route={item.avatar} key={index} />
-            );
-          })}
-        </div>
-        <Link
-          href={"/pesquisa"}
-          className="self-center w-3/4 lg:w-2/3 text-white bg-secundaria-100 hover:bg-secundaria-200 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
-        >
-          {cards.search}
-        </Link>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-col  justify-center">
+                <div className="flex mb-5 p-1 -space-x-4 justify-center">
+                    {avatares?.map((item, index) => {
+                        return <Avatar route={item.avatar} key={index} />;
+                    })}
+                </div>
+                <Link
+                    href={"/pesquisa"}
+                    className="self-center w-3/4 lg:w-2/3 text-white bg-secundaria-100 hover:bg-secundaria-200 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+                >
+                    {cards.search}
+                </Link>
+            </div>
+        </>
+    );
 }
