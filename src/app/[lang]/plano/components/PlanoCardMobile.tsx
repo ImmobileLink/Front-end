9dd7eea156f6d81e66e944d23b5990a7a2163c86
BeatrizNataly <beatriznatyce@gmail.com>
2023-10-30 18:11:30 -0300

    Card premium corretor mobile ok

--- src/app/[lang]/plano/components/PlanoCardMobile.tsx
+++ src/app/[lang]/plano/components/PlanoCardMobile.tsx
@@ -102,6 +102,79 @@ export default function PlanoCardMobile({
                 // PREMIUM TIER - Corretor
                 // ...
             }
+            <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+                    PREMIUM
+                </h5>
+                <div className="flex items-baseline text-gray-900 dark:text-white">
+                    <span className="text-5xl font-semibold text-blue-700">
+                        {dict.freetier}
+                    </span>
+                    <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                        <a className="line-through">{sub.brokertier}</a>
+                    </span>
+                </div>
+                <ul role="list" className="space-y-5 my-7">
+                    <li className="flex space-x-3 items-center">
+                        <BsCheckCircle className="text-green-500" />
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {dict.feedbackreview}
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <BsCheckCircle className="text-green-500" />
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {dict.feed}
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <BsCheckCircle className="text-green-500" />
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {dict.companyafiliation}
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <BsCheckCircle className="text-green-500" />
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {dict.chat}
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <BsCheckCircle className="text-green-500" />
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {dict.dashboard}
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <BsCheckCircle className="text-green-500" />
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {dict.visibility}
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <BsCheckCircle className="text-green-500" />
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {dict.exclusivefunctions}
+                        </span>
+                    </li>
+                </ul>
+                {premium ? (
+                    <div className="p-6 flex justify-center text-blue-700 font-semibold">
+                        {sub.selected}
+                    </div>
+                ) : (
+                    <button
+                        type="button"
+                        className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                        onClick={() => setTogglePlano(true)}
+                    >
+                        {sub.select} Premium
+                    </button>
+                )}
+            </div>
+            {
+                // MODAL
+            }
             {togglePlano ? (
                 <ModalTogglePlano
                     id={id!}
