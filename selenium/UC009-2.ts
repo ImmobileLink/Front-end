9cadb839e8b69d8f1e6952ebc4038520eeadde6b
Gabriel Parreira <gabriel.s.parreira@gmail.com>
2023-11-05 16:22:04 -0300

    Teste UC009-2 pronto

--- selenium/UC009-2.ts
+++ selenium/UC009-2.ts
@@ -156,7 +156,7 @@ const {Builder, By, until, WebDriver, WebElement} = require('selenium-webdriver'
     }    
     finally {
         console.log("Test Ended.")
-        // await driver.quit();
+        await driver.quit();
     }
 })();
 
