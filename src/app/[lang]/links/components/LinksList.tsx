658903c6d596cab982fdcd9dc5e50ebfd9450bbb
BeatrizNataly <beatriznatyce@gmail.com>
2023-11-08 12:29:29 -0300

    componente de card meus links - part 2

--- src/app/[lang]/links/components/LinksList.tsx
+++ src/app/[lang]/links/components/LinksList.tsx
@@ -2,29 +2,106 @@
 
 import { Card } from "../../(components)/(compositions)/(card)";
 import Link from "next/link";
+import Links from "../components/Links"
+import { useState } from "react";
 import { BiSolidLeftArrow } from "react-icons/bi";
 
-interface LinksListProps {}
+interface LinksListProps {
+    links:
+        | {
+              id: string;
+              nome: string;
+              avatar: string;
+          }[]
+        | null
+        | undefined;
+    assoc:
+        | {
+              id: string;
+              nome: string;
+              avatar: string;
+          }[]
+        | null
+        | undefined;
+}
 
-export default function LinksList({}: LinksListProps) {
+export default function LinksList({ assoc, links }: LinksListProps) {
+    const [view, setView] = useState("links");
+    // TO DO:
+    // BUSCAR INFOS DE TODOS OS LINKS E ASSOCIADOS NO BD PARA DISPOR DA MESMA FORMA QUE ESTÁ NA BUSCA
     return (
         <div>
-            <Card.Root className="md:pr-8 p-2">
+            <Card.Root className="md:pr-8 p-2 pt-4">
                 <div className="flex items-center ml-6 text-xl">
                     <Link href="/feed" className="mr-4">
                         <BiSolidLeftArrow />
                     </Link>
-                    <a>Meus links</a>
-                    <a className="px-2">Associados</a>
+                    <a
+                        onClick={() => setView("links")}
+                        className={`${
+                            view == "links" ? "underline font-semibold" : ""
+                        } pr-2 cursor-pointer`}
+                    >
+                        Meus links
+                    </a>
+                    <a
+                        onClick={() => setView("assoc")}
+                        className={`${
+                            view == "assoc" ? "underline font-semibold" : ""
+                        } px-2 cursor-pointer`}
+                    >
+                        Associados
+                    </a>
                 </div>
-                {
-                    // mensagem padrão caso user sem links
-                }
                 <div className="ml-16 mt-2">
-                    <p>
-                        Parece que você ainda não tem nenhuma conexão. Tente
-                        encontrar novos Links.
-                    </p>
+                    {view == "links" ? (
+                        <>
+                            {links != null && links != undefined ? (
+                                <>
+                                    {links.length > 0 ? (
+                                        <>
+                                        
+                                            <Links usuario={links[0]}/>
+                                        
+                                        </>
+                                    ) : (
+                                        <p>
+                                            Parece que você ainda não tem
+                                            nenhuma conexão. Tente encontrar
+                                            novos Links.
+                                        </p>
+                                    )}
+                                </>
+                            ) : (
+                                <p>
+                                    Ocorreu um erro ao carregar seus Links. Por
+                                    favor, tente novamente mais tarde
+                                </p>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            {assoc != null && assoc != undefined ? (
+                                <>
+                                    {assoc.length > 0 ? (
+                                        <>EXIBE ARRAY ASSOCIAÇÕES</>
+                                    ) : (
+                                        <p>
+                                            Parece que você ainda não tem
+                                            nenhuma associação. Tente encontrar
+                                            novos parceiros.
+                                        </p>
+                                    )}
+                                </>
+                            ) : (
+                                <p>
+                                    Ocorreu um erro ao carregar suas
+                                    associações. Por favor, tente novamente mais
+                                    tarde
+                                </p>
+                            )}
+                        </>
+                    )}
                 </div>
             </Card.Root>
         </div>
--- src/app/[lang]/links/page.tsx
+++ src/app/[lang]/links/page.tsx
@@ -64,7 +64,7 @@ export default async function page({ params: { lang } }: pageProps) {
             />
             <div className="pt-4 w-full h-fit min-h-screen bg-branco dark:bg-dark-200 flex flex-row justify-center">
                 <div className="md:mx-2 md:w-7/12 w-11/12 flex">
-                    <LinksList/>
+                    <LinksList links={userData.links} assoc={userData.assoc}/>
                 </div>
                 <div className="w-fit hidden md:flex mx-4">
                     <Card.Root>
